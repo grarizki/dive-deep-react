e732c92681de55ce4b2cdc4c2f14fbafdfa75a29
grarizki <28901232+grarizki@users.noreply.github.com>
2021-09-25 03:11:51 +0700

    add box component

--- day-1/src/box-exercise/boxComponent.js
+++ day-1/src/box-exercise/boxComponent.js
@@ -1,13 +1,150 @@
+// import React from "react";
+// import "./box.css";
+
+// const BoxColourFulContext = React.createContext({
+//   boxTypeItems: [],
+//   addBoxToArray: (boxType) => {},
+//   removeBoxFromArray: (indexToDelete) => {}
+// });
+
+// class Box extends React.Component {
+//   render() {
+//     let styleBox = "box";
+
+//     switch (this.props.type) {
+//       case "small":
+//         styleBox = styleBox + " box--small";
+//         break;
+//       case "medium":
+//         styleBox = styleBox + " box--medium";
+//         break;
+//       case "large":
+//         styleBox = styleBox + " box--large";
+//         break;
+//       default:
+//     }
+
+//     return (
+//       <BoxColourFulContext.Consumer>
+//         {(context) => {
+//           return (
+//             <div className={styleBox} style={{ fontStyle: "italic" }}>
+//               <div style={{ flex: 1, justifyContent: "flex-end" }}>{`${
+//                 this.props.indexArray + 1
+//               }. ${this.props.type} box`}</div>
+//               <div>
+//                 <button
+//                   style={{
+//                     backgroundColor: "red",
+//                     color: "white",
+//                     width: "100%"
+//                   }}
+//                   onClick={() => {
+//                     context.removeBoxFromArray(this.props.indexArray);
+//                   }}
+//                 >
+//                   delete
+//                 </button>
+//               </div>
+//             </div>
+//           );
+//         }}
+//       </BoxColourFulContext.Consumer>
+//     );
+//   }
+// }
+
+// class ColourFulBoxComponent extends React.Component {
+//   state = {
+//     selectedBoxType: "small"
+//   };
+
+//   render() {
+//     return (
+//       <BoxColourFulContext.Consumer>
+//         {(context) => {
+//           return (
+//             <div style={{ display: "flex" }}>
+//               <div style={{ flex: 1 }}>
+//                 {context.boxTypeItems.map((boxType, index) => (
+//                   <Box
+//                     key={index.toString()}
+//                     type={boxType}
+//                     indexArray={index}
+//                   />
+//                 ))}
+//               </div>
+//               <div style={{ flex: 1 }}>
+//                 <select
+//                   name="box"
+//                   id="boxstype"
+//                   onChange={(event) => {
+//                     this.setState({ selectedBoxType: event.target.value });
+//                   }}
+//                 >
+//                   <option value="small">small</option>
+//                   <option value="medium">medium</option>
+//                   <option value="large">large</option>
+//                 </select>
+//                 <button
+//                   onClick={() => {
+//                     context.addBoxToArray(this.state.selectedBoxType);
+//                   }}
+//                 >
+//                   +
+//                 </button>
+//               </div>
+//             </div>
+//           );
+//         }}
+//       </BoxColourFulContext.Consumer>
+//     );
+//   }
+// }
+
+// class ColourFulBoxContainerComponent extends React.Component {
+//   state = { boxTypeItems: ["small", "medium", "large"] };
+
+//   addBoxToArray = (boxType) => {
+//     this.setState({
+//       boxTypeItems: [...this.state.boxTypeItems, boxType]
+//     });
+//   };
+
+//   removeBoxFromArray = (indexToDelete) => {
+//     const newArray = this.state.boxTypeItems.filter(
+//       (_, index) => index !== indexToDelete
+//     );
+//     this.setState({ boxTypeItems: newArray });
+//   };
+
+//   render = () => {
+//     return (
+//       <BoxColourFulContext.Provider
+//         value={{
+//           boxTypeItems: this.state.boxTypeItems,
+//           addBoxToArray: this.addBoxToArray,
+//           removeBoxFromArray: this.removeBoxFromArray
+//         }}
+//       >
+//         <ColourFulBoxComponent />
+//       </BoxColourFulContext.Provider>
+//     );
+//   };
+// }
+
+// export default ColourFulBoxContainerComponent;
+
 import React from "react";
 import "./box.css";
 
-const BoxContext = React.createContext({
-  arrayBox: [],
-  addBoxToArrayBox: () => {},
-  removeArrayBoxBasedIndex: () => {},
+const BoxColourFulContext = React.createContext({
+  boxTypeItems: [],
+  addBoxToArray: (boxType) => {},
+  removeBoxFromArray: (indexToDelete) => {}
 });
 
-class BoxComponent extends React.Component {
+class Box extends React.Component {
   render() {
     let styleBox = "box";
 
@@ -25,17 +162,22 @@ class BoxComponent extends React.Component {
     }
 
     return (
-      <BoxContext.Consumer>
+      <BoxColourFulContext.Consumer>
         {(context) => {
-          console.log("value >> ", context);
           return (
-            <div className={styleBox}>
+            <div className={styleBox} style={{ fontStyle: "italic" }}>
+              <div style={{ flex: 1, justifyContent: "flex-end" }}>{`${
+                this.props.indexArray + 1
+              }. ${this.props.type} box`}</div>
               <div>
-                {this.props.type + " box"}
                 <button
-                  style={{ backgroundColor: "red" }}
+                  style={{
+                    backgroundColor: "red",
+                    color: "white",
+                    width: "100%"
+                  }}
                   onClick={() => {
-                    this.props.removeBox(this.props.indexArray);
+                    context.removeBoxFromArray(this.props.indexArray);
                   }}
                 >
                   delete
@@ -44,96 +186,88 @@ class BoxComponent extends React.Component {
             </div>
           );
         }}
-      </BoxContext.Consumer>
+      </BoxColourFulContext.Consumer>
     );
   }
 }
 
 class ColourFulBoxComponent extends React.Component {
   state = {
-    boxTypeItems: ["small", "medium", "large"],
-    selectedBoxType: "small",
+    selectedBoxType: "small"
   };
 
-  removeBoxBasedOnIndexArray = (indexToDelete) => {
+  render() {
+    return (
+      <BoxColourFulContext.Consumer>
+        {(context) => {
+          return (
+            <div style={{ display: "flex" }}>
+              <div style={{ flex: 1 }}>
+                {context.boxTypeItems.map((boxType, index) => (
+                  <Box
+                    key={index.toString()}
+                    type={boxType}
+                    indexArray={index}
+                  />
+                ))}
+              </div>
+              <div style={{ flex: 1 }}>
+                <select
+                  name="box"
+                  id="boxstype"
+                  onChange={(event) => {
+                    this.setState({ selectedBoxType: event.target.value });
+                  }}
+                >
+                  <option value="small">small</option>
+                  <option value="medium">medium</option>
+                  <option value="large">large</option>
+                </select>
+                <button
+                  onClick={() => {
+                    context.addBoxToArray(this.state.selectedBoxType);
+                  }}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+          );
+        }}
+      </BoxColourFulContext.Consumer>
+    );
+  }
+}
+
+class ColourFulBoxContainerComponent extends React.Component {
+  state = { boxTypeItems: ["small", "medium", "large"] };
+
+  addBoxToArray = (boxType) => {
+    this.setState({
+      boxTypeItems: [...this.state.boxTypeItems, boxType]
+    });
+  };
+
+  removeBoxFromArray = (indexToDelete) => {
     const newArray = this.state.boxTypeItems.filter(
       (_, index) => index !== indexToDelete
     );
     this.setState({ boxTypeItems: newArray });
   };
 
-  render() {
+  render = () => {
     return (
-      <div style={{ display: "flex" }}>
-        <div style={{ flex: 1 }}>
-          {this.state.boxTypeItems.map((boxType, index) => (
-            <BoxComponent
-              key={index.toString()}
-              type={boxType}
-              removeBox={this.removeBoxBasedOnIndexArray}
-              indexArray={index}
-            />
-          ))}
-        </div>
-        <div style={{ flex: 1 }}>
-          <select
-            name="box"
-            id="boxstype"
-            onChange={(event) => {
-              this.setState({ selectedBoxType: event.target.value });
-            }}
-          >
-            <option value="small">small</option>
-            <option value="medium">medium</option>
-            <option value="large">large</option>
-          </select>
-          <button
-            onClick={() => {
-              this.setState({
-                boxTypeItems: [
-                  ...this.state.boxTypeItems,
-                  this.state.selectedBoxType,
-                ],
-              });
-            }}
-          >
-            +
-          </button>
-        </div>
-      </div>
-    );
-  }
-}
-
-class BoxContainer extends React.Component {
-  state = { arrayBox: [] };
-
-  addBoxToArrayBox = (boxType) => {
-    this.setState({
-      arrayBox: [...this.state.arrayBox, boxType],
-    });
-  };
-
-  removeArrayBoxBasedIndex = (indexToDelete) => {
-    const newArray = this.state.boxTypes.filter(
-      (_, index) => index !== indexToDelete
-    );
-    this.setState({ arrayBox: newArray });
-  };
-
-  render() {
-    return (
-      <BoxContext.Provider
+      <BoxColourFulContext.Provider
         value={{
-          arrayBox: this.state.arrayBox,
-          addBoxToArrayBox: this.addBoxToArrayBox,
-          removeArrayBoxBasedIndex: this.removeArrayBoxBasedIndex,
+          boxTypeItems: this.state.boxTypeItems,
+          addBoxToArray: this.addBoxToArray,
+          removeBoxFromArray: this.removeBoxFromArray
         }}
       >
         <ColourFulBoxComponent />
-      </BoxContext.Provider>
+      </BoxColourFulContext.Provider>
     );
-  }
+  };
 }
 
-export default BoxContainer;
+export default ColourFulBoxContainerComponent;
